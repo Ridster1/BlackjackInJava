16127e8a5f705c816f42fdbb1f01fbc4cd291b7f
Ridster1 <62082505+Ridster1@users.noreply.github.com>
2020-03-18 06:53:01 -0400

    Update Deck.java

--- Deck.java
+++ Deck.java
@@ -1,3 +1,4 @@
+
 package blackjackinjava;
 import java.util.*;
 
@@ -8,6 +9,7 @@ class Deck {
     private enum Rank {
         TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7), EIGHT(8),
         NINE(9), TEN(10), JACK(10),QUEEN(10), KING(10), ACE(11);
+        
   
         private int value;
         private Rank (int value) {
@@ -52,10 +54,22 @@ class Deck {
             }
         }
         return cardVal;
-    } 
+    }    
+        
+    public String getCardRank(Object c) {
+        ArrayList rank= (ArrayList) c;
+        String sRank = rank.get(0).toString();  
+        return sRank;
+    }
+   
+        
+   public String getCardSuit(Object d) {
+        ArrayList suit= (ArrayList) d;
+        String sSuit = suit.get(1).toString();  
+        return sSuit;
+   }    
+           
 }
      
-
-
 
      
